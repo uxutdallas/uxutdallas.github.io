e0ad23a22764a006bbab2b03d09cc9cb76f1ae72
ReynaDuong <minhduong.md25@gmail.com>
2019-04-03 18:48:20 -0500

    contact form and layout for team and events

---
+++ src/script/templates/templates.js
@@ -0,0 +1,20 @@
+const recentEventsTemplate =
+    `<div class="row ">
+        <div class="d-flex flex-column flex-md-row-reverse event">
+              <div class="align-self-center text-center flex-fill">
+                <img src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/45153596_1067944116700688_363339308230246400_o.jpg?_nc_cat=111&_nc_ht=scontent-dfw5-2.xx&oh=d87ea90d46ac17cf5dd9a38a09c2577a&oe=5C6F4550">
+              </div>
+              <div class="align-self-center font-weight-light flex-fill text-content">
+                <h3 class="font-weight-normal event-name"></h3>
+                <p>Wednesday, November 7 | 7 - 9 pm | ATC 2.918</p>
+                <p class="event-description"></p>
+                    <a href="https://www.facebook.com/pg/uxutd/events/"
+                       class="btn btn-gradient">More Events</a>
+                </div>                  
+        </div>
+     </div>`;
+
+const skillGroupTemplate = `<div class="row skillGroup">
+								<div class="col-lg-4 skillGroupName"></div>
+								<div class="col-lg-8 skillGroupValue"></div>
+							</div>`;
