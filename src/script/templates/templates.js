c57de5b8f7e91536d28222d76e52b772ff702fee
ReynaDuong <minhduong.md25@gmail.com>
2019-04-24 12:35:57 -0500

    templates and data

--- src/script/templates/templates.js
+++ src/script/templates/templates.js
@@ -1,18 +1,28 @@
-const recentEventsTemplate =
-    `<div class="row ">
-        <div class="d-flex flex-column flex-md-row-reverse event">
-              <div class="align-self-center text-center flex-fill">
-                <img src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/45153596_1067944116700688_363339308230246400_o.jpg?_nc_cat=111&_nc_ht=scontent-dfw5-2.xx&oh=d87ea90d46ac17cf5dd9a38a09c2577a&oe=5C6F4550">
-              </div>
-              <div class="align-self-center font-weight-light flex-fill text-content">
-                <h3 class="font-weight-normal event-name"></h3>
-                <p class="event-info">Wednesday, November 7 | 7 - 9 pm | ATC 2.918</p>
-                <p class="event-description"></p>
+const eventTemplate =
+    ` <div class="container">
+            <div class="d-flex flex-column flex-md-row-reverse event">
+                <div class="align-self-center text-center flex-fill">
+                    <img src="" alt="">
+                </div>
+                <div class="align-self-center font-weight-light flex-fill text-content">
+                    <h3 class="font-weight-normal">Wireframing 101</h3>
+                    <p>Wednesday, November 7 | 7 - 9 pm | ATC 2.918</p>
+                    <p>
+                        Learn the basics of wireframing - an essential step in the UX design process.
+                    </p>
+                    <p>
+                        Join us for a workshop where we'll talk about wireframing - the laying out of apps & websites -
+                        including how to do it and why it's important. Practice by redesigning an app or website that
+                        you already know, like Facebook or Snapchat!
+                    </p>
+                    <p>
+                        The workshop will be held at ATC 2.918 from 7 to 9 PM on November 7. Please RSVP on Facebook!
+                    </p>
                     <a href="https://www.facebook.com/pg/uxutd/events/"
-                       class="btn btn-gradient">More Events</a>
-                </div>                  
-        </div>
-     </div>`;
+                       class="btn btn-gradient">Learn More</a>
+                </div>
+            </div>
+        </div>`;
 
 
 
@@ -21,4 +31,5 @@ const skillGroupTemplate = `<div class="row skillGroup">
 								<div class="col-lg-4 skillGroupName"></div>
 								<div class="col-lg-8 skillGroupValue"></div>
 							</div>`;
+
 
