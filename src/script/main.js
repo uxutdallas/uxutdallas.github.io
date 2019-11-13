5fb003408505803c220f76c2026fe214dc4b30b5
Minh Duong <minhduong.md25@gmail.com>
2019-11-12 23:26:41 -0600

    UXperience page added (#9)
    * minor changes in photos
    delete unnecessary files
    
    * minor changes in photos
    delete unnecessary files
    
    * create a contact page
    
    * update officer roles
    
    * contact page updated
    
    * uxperience page initial commit
    
    * uxperience page sponsors and guest speakers
    
    * code format
    
    * new form for uxperience page
    
    * Fix broken links in About page + fix background images
    
    * Add event template and map for uxperience
    
    * update officers' photos and feed content to UXperience page
    
    * update presenters' information
    
    * link to the image

--- src/script/main.js
+++ src/script/main.js
@@ -1,14 +1,11 @@
-var mainContent = document.querySelector('#main');
-var eventsLink = document.querySelector('.events-link');
+let mainContent = document.querySelector('#main');
+let eventsLink = document.querySelector('.events-link');
 
 eventsLink.addEventListener('click', function (e) {
-  e.preventDefault();
-  mainContent.scrollIntoView({ 
-    behavior: 'smooth',
-    block: 'start',
-    inline: 'nearest'
-  });
+    e.preventDefault();
+    mainContent.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
 });
-
-
-
